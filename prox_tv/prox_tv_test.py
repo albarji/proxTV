3cb889a6ef5503f9a4b3e999ce01ec6e88f0e832
alvaro <albarjip@gmail.com>
2015-05-30 12:42:36 +0200

    Solves #9 Added type checks to avoid issues when integer arguments are provided. Additional python tests.

--- prox_tv/prox_tv_test.py
+++ prox_tv/prox_tv_test.py
@@ -13,6 +13,28 @@ def test_tv1w_1d():
         solutions = [tv1w_1d(x, w, method=method) for method in methods]
         for i in range(len(solutions)-1):
             assert np.allclose(solutions[i], solutions[i+1])
+            
+            
+def test_tv1w_1d_uniform_weights():
+    for _ in range(20):
+        dimension = np.random.randint(1e1, 1e3)
+        x = 100*np.random.randn(dimension)
+        w1 = np.random.rand()
+        w = np.ones(dimension-1) * w1 
+        solw = tv1w_1d(x, w)
+        sol = tv1_1d(x, w1)
+        assert np.allclose(solw, sol)
+        
+        
+def test_tv1w_1d_uniform_weights_small_input():
+    for _ in range(1000):
+        dimension = np.random.randint(2, 4)
+        x = 100*np.random.randn(dimension)
+        w1 = np.random.rand()
+        w = np.ones(dimension-1) * w1 
+        solw = tv1w_1d(x, w)
+        sol = tv1_1d(x, w1)
+        assert np.allclose(solw, sol)
 
 
 def test_tv1_1d():
@@ -65,3 +87,42 @@ def test_tv1_2d():
         solutions.append(tv1w_2d(x, w_cols, w_rows, max_iters=5000))
         for i in range(1, len(solutions)):
             assert np.allclose(solutions[i], solutions[0], atol=1e-3)
+
+
+def test_tv1w_2d_uniform_weights():
+    for _ in range(20):
+        rows = np.random.randint(1e1, 3e1)
+        cols = np.random.randint(1e1, 3e1)
+        x = 100*np.random.randn(rows, cols)
+        w1 = np.random.rand()
+        w_rows = np.ones([rows-1, cols]) * w1 
+        w_cols = np.ones([rows, cols-1]) * w1 
+        solw = tv1w_2d(x, w_rows, w_cols, max_iters=5000)
+        solw1 = tv1_2d(x, w1, max_iters=5000)
+        assert np.allclose(solw, solw1, atol=1e-3)
+        
+
+def test_tv1w_2d_uniform_weights_small_input():
+    for _ in range(1000):
+        rows = np.random.randint(2, 4)
+        cols = np.random.randint(2, 4)
+        x = 100*np.random.randn(rows, cols)
+        w1 = np.random.rand()
+        w_rows = np.ones([rows-1, cols]) * w1 
+        w_cols = np.ones([rows, cols-1]) * w1 
+        solw = tv1w_2d(x, w_rows, w_cols, max_iters=5000)
+        solw1 = tv1_2d(x, w1, max_iters=5000)
+        assert np.allclose(solw, solw1, atol=1e-3)
+        
+        
+def test_tv1w_2d_emengd():
+    r"""Issue reported by emengd
+    
+    Make the solver fail due to missing checks on integer arguments
+    """        
+    a = -np.array([[1,2,3],[4,5,6],[7,8,9]])/10.
+    sol1 = tv1w_2d(a, np.array([[1,1,1],[1,1,1]]), 
+                   np.array([[1,1],[1,1],[1,1]]), max_iters=100)
+    sol2 = tv1_2d(a, 1)
+    assert np.allclose(sol1, sol2, atol=1e-3)
+    
