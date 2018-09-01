ceb43f046b36106f5b7148e64aa60d8a683a74a1
Álvaro Barbero Jiménez <albarjip@gmail.com>
2018-09-01 22:32:10 +0200

    Missing script

---
+++ .travis/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ev
+cd ..
+
+# Linux build and install
+if [ "${TRAVIS_OS_NAME}" == "linux" ]
+then
+    docker pull quay.io/pypa/manylinux1_x86_64
+    PYTHONVERSION=$(echo ${TRAVIS_PYTHON_VERSION} | tr -d .)
+    PACKAGING=$([[ ${PYTHONVERSION} = "27" ]] && echo "mu" || echo "m")
+    # Build precompiled python wheels
+    docker run --rm -it -v $(pwd):/io quay.io/pypa/manylinux1_x86_64 bash -c "cd /io; bash buildwheels.sh"
+    # Install wheel for this with version
+    pip install wheelhouse/prox_tv-*-cp${PYTHONVERSION}-cp${PYTHONVERSION}${PACKAGING}-manylinux1_x86_64.whl
+
+# Mac build and install
+elif [ "${TRAVIS_OS_NAME}" == "osx" ]
+then
+    wheel . -w wheelhouse/
+    ls wheelhouse
+    # TODO: install
+fi
