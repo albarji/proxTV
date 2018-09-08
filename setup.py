acf03ffa53a21fd9dafd4d5b5d81cde5e25b2f04
Álvaro Barbero Jiménez <albarjip@gmail.com>
2018-09-08 13:03:31 +0200

    test PyPI autodeploy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="prox_tv",
-    version="3.2.12",
+    version="3.2.13",
     description="Toolbox for fast Total Variation proximity operators",
     long_description="""
 proxTV is a toolbox implementing blazing fast implementations of Total Variation
