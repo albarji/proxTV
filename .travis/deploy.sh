a36ea62533100a19eea0b3e4a82c417d3fb680bc
Álvaro Barbero Jiménez <albarjip@gmail.com>
2018-10-16 20:00:02 +0100

    Deploy only on tags

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -v
 
-# TODO: run only for tags
-# TODO: use real PyPI server
-
 # Deploys built wheels to PyPI
-pip install twine
-ls -l dist/*  # List files to upload
-twine upload --repository-url https://test.pypi.org/legacy/ --username albarji dist/*
+if [[ ! -z "$TRAVIS_TAG" ]]
+then
+    pip install twine
+    ls -l dist/*  # List files to upload
+    twine upload --username albarji dist/*
+fi
